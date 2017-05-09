43965d2069b951e75a4fb70368cce43f4b0d36f9
Masakazu Hayatsu <masasnap@users.noreply.github.com>
2017-05-09 23:18:53 +0900

    :new: add note about sample files of flow-guide (contd.)

--- src/flow-guide/tutorial/00-basics/07-import.js
+++ src/flow-guide/tutorial/00-basics/07-import.js
@@ -3,7 +3,7 @@
 import { convertUnit } from './06-export';
 
 // $FlowExpectedError: Flow recognized non-existent symbols and even recognizes typos!
-import { conveRtUnit } from './06-export';
+// import { conveRtUnit } from './06-export';
 
 // Types are imported independently from 'real' code
 // Flow kinda doesn't differentiate between 'real' and 'type' code
