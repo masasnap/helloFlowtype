b7b60252c4c5ecc64f5bb1c14149a85dd60a1418
Masakazu Hayatsu <masasnap@users.noreply.github.com>
2017-05-11 23:57:09 +0900

    :new: add note about sample files of flow_introduction

--- src/flow_introduction/example2.js
+++ src/flow_introduction/example2.js
@@ -1,10 +1,17 @@
 // @flow
 // 返り値の例
 
-function f1(): string {
-  return Math.random() < 0.5 ? 'test': null;  // nullの場合があるでのエラー。返り値を string|null か ?string にする。後者はundefinedを許容する。
+// function f1(): string {
+//   return Math.random() < 0.5 ? 'test': null;  // nullの場合があるでのエラー。返り値を string|null か ?string にする。後者はundefinedを許容する。
+// }
+// f1();
+
+function f2(): string|null {
+  return Math.random() < 0.5 ? 'test': null;
 }
+f2();
 
-f1();
-
-
+function f3(): ?string {
+  return Math.random() < 0.5 ? 'test': null;
+}
+f3();
