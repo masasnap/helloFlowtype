9759d9f5107feefc3df224abe4817eb71af41a7e
Masakazu Hayatsu <masasnap@users.noreply.github.com>
2017-05-12 00:05:43 +0900

    :new: add note about sample files of flow_introduction (contd.)

--- src/flow_introduction/example3.js
+++ src/flow_introduction/example3.js
@@ -9,17 +9,17 @@ function f2(s: string) {
   console.log(s);
 }
 
-const v = f1(); // vには明示的に型をしていしないが、f1の返り値なのでstring|nullとみなされる
-f2(v);  // vがstring|nullで、f2の引数がstringなのでエラー
+// const v = f1(); // vには明示的に型をしていしないが、f1の返り値なのでstring|nullとみなされる
+// f2(v);  // vがstring|nullで、f2の引数がstringなのでエラー
 
 
-// // キャストもある(f1が引数に依存してその状況ではnullを返さない場合などに利用)
-// const v = ((f1(): any): string); // キャスト。 https://flowtype.org/blog/2015/02/18/Typecasts.html
-// f2(v);
+// キャストもある(f1が引数に依存してその状況ではnullを返さない場合などに利用)
+const v1 = ((f1(): any): string); // キャスト。 https://flowtype.org/blog/2015/02/18/Typecasts.html
+f2(v1);
 
 
-// // 条件文を考慮してくれる
-// const v = f1(); // ここではvはstring|nullだが、
-// if (v !== null) { // この中にくるのはstringの場合なので、
-//   f2(v);  // これはエラーにならない。
-// }
+// 条件文を考慮してくれる
+const v2 = f1(); // ここではvはstring|nullだが、
+if (v2 !== null) { // この中にくるのはstringの場合なので、
+  f2(v2);  // これはエラーにならない。
+}
--- src/flow_introduction/note.md
+++
@@ -1,9 +0,0 @@
-# Note
-
-## example1.js
-
-## example2.js
-
-## example3.js
-
-## example4.js
